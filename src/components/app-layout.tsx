aba9ea47e8ded54c8280de8c3e0bf31362bb3d25
italloVidal <itallovidalll@gmail.com>
2024-10-03 15:46:50 -0300

    add inter font

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,14 +1,16 @@
 import React, { ReactNode } from 'react'
 import { Footer } from '@/components/footer/footer'
 import { Navbar } from '@/components/navbar'
+import { Inter } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className={inter.className}>
       <Navbar />
-      <main className={'m-auto max-w-safe'}>{children}</main>
+      <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
