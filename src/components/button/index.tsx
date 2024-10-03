8e2e021cbb34ffed310e3802a94e58f3f1aa174c
italloVidal <itallovidalll@gmail.com>
2024-10-03 11:47:56 -0300

    create button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,21 @@
+import { ComponentType } from 'react'
+import { LucideProps } from 'lucide-react'
+import { Button as ShadButton, ButtonProps } from '@/components/ui/button'
+import Link from 'next/link'
+
+interface NavButton extends ButtonProps {
+  Icon?: ComponentType<LucideProps>
+  children?: string
+  href: string
+}
+
+export function Button({ href, Icon, variant, children, ...props }: NavButton) {
+  return (
+    <ShadButton variant={variant} asChild {...props}>
+      <Link href={href} className={'gap-2'}>
+        {Icon && <Icon />}
+        {children && children}
+      </Link>
+    </ShadButton>
+  )
+}
