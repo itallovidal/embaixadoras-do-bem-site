76a43f0b0297e34beac562e3b75c4a79193540e9
italloVidal <itallovidalll@gmail.com>
2024-10-23 15:49:55 -0300

    fix project details skeleton

--- src/pages/projects/[id]/index.tsx
+++ src/pages/projects/[id]/index.tsx
@@ -9,7 +9,7 @@ import { ArrowLeft } from 'lucide-react'
 import { Button } from '@/presentation/components/global-components/button'
 import { convertSecondsToDate } from '@/presentation/utils/convert-seconds-to-date'
 import { ShowProjectStatus } from '@/presentation/components/pages-components/project/show-project-status'
-import { ProjectCardSkeleton } from '@/presentation/components/skeletons/project-details-skeleton'
+import { ProjectDetailsSkeleton } from '@/presentation/components/skeletons/project-details-skeleton'
 function Index() {
   const router = useRouter()
   const { id } = router.query as { id: string }
@@ -29,58 +29,63 @@ function Index() {
         .split(',')[0]
     : undefined
 
-  if (isLoading) return <ProjectCardSkeleton />
+  return (
+    <article className={'my-12 animate-showing opacity-0'}>
+      <section className={'max-w-safeMobile xl:max-w-safeDesktop m-auto'}>
+        {!isLoading && <ProjectDetailsSkeleton />}
 
-  if (project)
-    return (
-      <article className={'my-12 animate-showing opacity-0'}>
-        <section className={'max-w-safeMobile xl:max-w-safeDesktop m-auto'}>
-          <div
-            className={
-              'flex flex-col-reverse items-start justify-between sm:flex-row sm:gap-5 sm:items-center'
-            }
-          >
-            <Heading>{project.title}</Heading>
+        {project && (
+          <>
+            <div
+              className={
+                'flex flex-col-reverse items-start justify-between sm:flex-row sm:gap-5 sm:items-center'
+              }
+            >
+              <Heading>{project.title}</Heading>
 
-            <div className={'flex mb-12 justify-end sm:my-5'}>
-              <Button
-                onClick={() => router.back()}
-                Icon={ArrowLeft}
-                variant={'ghost'}
-              >
-                Voltar
-              </Button>
+              <div className={'flex mb-12 justify-end sm:my-5'}>
+                <Button
+                  onClick={() => router.back()}
+                  Icon={ArrowLeft}
+                  variant={'ghost'}
+                >
+                  Voltar
+                </Button>
+              </div>
             </div>
-          </div>
-          <Paragraph>{project.description}</Paragraph>
+            <Paragraph>{project.description}</Paragraph>
 
-          <ShowProjectStatus
-            project={{ isActive: project?.isActive, startDate, endDate }}
-          />
+            <ShowProjectStatus
+              project={{ isActive: project?.isActive, startDate, endDate }}
+            />
 
-          <div
-            className={'mt-8 flex flex-col flex-wrap w-full gap-4 sm:flex-row'}
-          >
-            {project.images.map((image, i) => (
-              <picture
-                className={
-                  'overflow-hidden rounded-2xl sm:w-[calc(theme(width.1/2)-theme(gap.2))]'
-                }
-              >
-                <Image
-                  className={'block w-full h-full object-cover'}
-                  key={i}
-                  src={image}
-                  width={370}
-                  height={600}
-                  alt={'Imagem do projeto'}
-                />
-              </picture>
-            ))}
-          </div>
-        </section>
-      </article>
-    )
+            <div
+              className={
+                'mt-8 flex flex-col flex-wrap w-full gap-4 sm:flex-row'
+              }
+            >
+              {project.images.map((image, i) => (
+                <picture
+                  className={
+                    'overflow-hidden rounded-2xl sm:w-[calc(theme(width.1/2)-theme(gap.2))]'
+                  }
+                >
+                  <Image
+                    className={'block w-full h-full object-cover'}
+                    key={i}
+                    src={image}
+                    width={370}
+                    height={600}
+                    alt={'Imagem do projeto'}
+                  />
+                </picture>
+              ))}
+            </div>
+          </>
+        )}
+      </section>
+    </article>
+  )
 }
 
 export default Index
--- src/presentation/components/skeletons/project-details-skeleton.tsx
+++ src/presentation/components/skeletons/project-details-skeleton.tsx
@@ -1,26 +1,27 @@
 import { Skeleton } from '@/presentation/components/shadcn-ui/skeleton'
 
-export function ProjectCardSkeleton() {
+export function ProjectDetailsSkeleton() {
   return (
-    <article className={'my-12 animate-showing opacity-0'}>
-      <section className={'max-w-safeMobile xl:max-w-safeDesktop m-auto'}>
-        <Skeleton className="h-6 w-[600px] rounded-l" />
-        <Skeleton className="h-32 w-[800px] my-4" />
-        <div className="space-y-2 mb-4">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
+    <>
+      <Skeleton className="h-6 w-[50%] rounded-l" />
+      <Skeleton className="h-32 w-[80%] my-4" />
+      <div className="space-y-2 mb-4">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
 
-        <div
-          className={
-            'mt-8 flex flex-col flex-wrap w-full gap-[1rem]  sm:flex-row'
-          }
-        >
-          {Array.from({ length: 4 }).map(() => (
-            <Skeleton className="h-[500px] w-[calc(50%-1rem)]" />
-          ))}
-        </div>
-      </section>
-    </article>
+      <div
+        className={
+          'mt-8 flex flex-col flex-wrap w-full gap-[1rem] sm:flex-row'
+        }
+      >
+        {Array.from({ length: 4 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            className="h-[500px] w-full sm:w-[calc(50%-1rem)]"
+          />
+        ))}
+      </div>
+    </>
   )
 }
