8361752ba43b7777d1a1d3d0f8e9d8df20960239
italloVidal <itallovidalll@gmail.com>
2024-11-08 20:20:27 -0300

    axios fix

--- src/infra/lib/axios/axios.ts
+++ src/infra/lib/axios/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const host =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://embaixadoras-do-bem-site.vercel.app'
+    : 'https://embaixadorasdobem.org/'
 
 export const api = axios.create({
   baseURL: `${host}/api`,
