17fe725602c291a7426359691e3e3604d41a1a61
Itallo Vidal <itallovidalll@gmail.com>
2025-03-25 13:21:44 -0300

    fix margin class wrong sequence

--- src/presentation/components/global-components/text/paragraph.tsx
+++ src/presentation/components/global-components/text/paragraph.tsx
@@ -1,16 +1,16 @@
-import { HTMLProps } from 'react'
+import { HTMLProps } from "react";
 
 interface IProps extends HTMLProps<HTMLParagraphElement> {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 export function Paragraph({ children, className, ...rest }: IProps) {
   return (
     <p
-      className={`text-md leading-7  ${!className?.includes('text-center') ? 'text-justify' : 'text-center'} ${className} my-6`}
+      className={`text-md leading-7  ${!className?.includes("text-center") ? "text-justify" : "text-center"} my-6 ${className} `}
       {...rest}
-      style={{ wordSpacing: '-2px' }}
+      style={{ wordSpacing: "-2px" }}
     >
       {children}
     </p>
-  )
+  );
 }
