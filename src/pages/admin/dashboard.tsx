3eb31aae37e1441c7fc105b92482b04129702e76
Itallo Vidal <itallovidalll@gmail.com>
2025-03-25 13:20:53 -0300

    add collab panel in admin dashboard

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,18 +1,20 @@
-import { ProjectManager } from '@/presentation/components/pages-components/admin/project-manager'
-import { getServerSideProps } from '@/gssp-admin-cookies'
-import { BlogManager } from '@/presentation/components/pages-components/admin/blog-manager'
+import { ProjectManager } from "@/presentation/components/pages-components/admin/project-manager";
+import { getServerSideProps } from "@/gssp-admin-cookies";
+import { BlogManager } from "@/presentation/components/pages-components/admin/blog-manager";
+import { CollaboratorManager } from "@/presentation/components/pages-components/admin/collaborator-manager";
 
 function Dashboard() {
   return (
     <div
-      className={'max-w-safeMobile xl:max-w-safeDesktop m-auto min-h-[60vh]'}
+      className={"max-w-safeMobile xl:max-w-safeDesktop m-auto min-h-[60vh]"}
     >
       <ProjectManager />
       <BlogManager />
+      <CollaboratorManager />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
 
-export { getServerSideProps }
+export { getServerSideProps };
