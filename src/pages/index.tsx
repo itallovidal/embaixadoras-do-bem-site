7006934213c3243aee231d0fe7aa791186721d6f
Itallo Vidal <itallovidalll@gmail.com>
2025-03-25 13:22:34 -0300

    add collab to consumer/client side of site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,28 @@
-import { Header } from '@/presentation/components/global-components/header/header'
-import Banner from '@/root/public/home/banner-home-background.png'
-import { About } from '@/presentation/components/pages-components/home/about'
-import { CallToAction } from '@/presentation/components/pages-components/home/call-to-action'
-import { Projects } from '@/presentation/components/pages-components/home/projects'
-import { Carousel } from '@/presentation/components/global-components/carousel/carousel'
-import { VolunteerSection } from '@/presentation/components/pages-components/home/volunteer-section'
-import { DonateSection } from '@/presentation/components/pages-components/home/donate-section'
-import { BlogPosts } from '@/presentation/components/pages-components/home/blog-posts'
-import CoreValues from '@/presentation/components/pages-components/home/core-values'
+import { Header } from "@/presentation/components/global-components/header/header";
+import Banner from "@/root/public/home/banner-home-background.png";
+import { About } from "@/presentation/components/pages-components/home/about";
+import { CallToAction } from "@/presentation/components/pages-components/home/call-to-action";
+import { Projects } from "@/presentation/components/pages-components/home/projects";
+import { Carousel } from "@/presentation/components/global-components/carousel/carousel";
+import { VolunteerSection } from "@/presentation/components/pages-components/home/volunteer-section";
+import { DonateSection } from "@/presentation/components/pages-components/home/donate-section";
+import { BlogPosts } from "@/presentation/components/pages-components/home/blog-posts";
+import CoreValues from "@/presentation/components/pages-components/home/core-values";
+import { CollabSection } from "@/presentation/components/pages-components/home/collab-section";
 
 export default function Index() {
   return (
-    <div className={'animate-showing opacity-0'}>
+    <div className={"animate-showing opacity-0"}>
       <Header
-        title={'Embaixadoras do Bem'}
-        alt={'Apoiando a causa'}
+        title={"Embaixadoras do Bem"}
+        alt={"Apoiando a causa"}
         img={Banner}
         description={
-          'Desde 2020 desenvolvendo campanhas de promoção e prevenção à Saúde.'
+          "Desde 2020 desenvolvendo campanhas de promoção e prevenção à Saúde."
         }
       />
 
-      <div className={'max-w-safeMobile xl:max-w-safeDesktop m-auto'}>
+      <div className={"max-w-safeMobile xl:max-w-safeDesktop m-auto"}>
         <About />
         <CoreValues />
         <Projects />
@@ -30,10 +31,11 @@ export default function Index() {
       <DonateSection />
       <VolunteerSection />
 
-      <div className={'max-w-safeMobile xl:max-w-safeDesktop m-auto'}>
+      <div className={"max-w-safeMobile xl:max-w-safeDesktop m-auto"}>
+        <CollabSection />
         <BlogPosts />
         <Carousel />
       </div>
     </div>
-  )
+  );
 }
